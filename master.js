b5e120fd23ffe4fbe2f8501891b78d7476fe0ee4
Andrea Sabatini <sabatini.andrea@outlook.it>
2021-04-05 23:11:56 +0200

    Piccoli ritocchi JS e CSS - Milestone 1 2 3 OK

--- master.js
+++ master.js
@@ -4,6 +4,7 @@ var app = new Vue({
     data: {
         index: 0,
         contacts: contacts,
+        message: '',
     },
     methods: {
         getTime: function (date) {
@@ -12,5 +13,32 @@ var app = new Vue({
             let minutes = datetime.getMinutes();
             return `${hours}:${minutes}`;
         },
-    },
+        getLastAccess: function (index) {
+            const messages = this.contacts[index].messages;
+            const lastIndex = messages.length - 1;
+
+            return messages[lastIndex].date;
+        },
+        send: function () {
+            const nowIndex = this.index
+            const date = dayjs().format('DD/MM/YYYY HH:mm:ss');
+            const message = {
+                date,
+				text: this.message,
+				status: 'sent',
+            }
+            this.contacts[nowIndex].messages.push(message);
+            this.message = '';
+
+            setTimeout(() => {
+                const date = dayjs().format('DD/MM/YYYY HH:mm:ss');
+                const message = {
+                    date,
+                    text: 'Ok! Mamy',
+                    status: 'received',
+                }
+                this.contacts[nowIndex].messages.push(message);
+            }, 1000);
+        }
+    }
 });
