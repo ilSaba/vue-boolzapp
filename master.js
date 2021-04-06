ecd542a737c5827185aa85f20ff078b9fd99b2e8
Andrea Sabatini <sabatini.andrea@outlook.it>
2021-04-06 22:58:00 +0200

    Esercizio completato, Milestone 5 compresa

--- master.js
+++ master.js
@@ -3,11 +3,24 @@ var app = new Vue({
     el: '#root',
     data: {
         index: 0,
+        indexMess: null,
         contacts: contacts,
         message: '',
         filter: '',
+        showWindow: false,
+        currentMessage: '',
     },
     methods: {
+        showMenu: function (index) {
+            if (this.showWindow) {
+                this.showWindow = false;
+                
+            } else {
+                this.showWindow = true;
+                this.currentMessage = index;
+            }
+            
+        },
         getTime: function (date) {
             let datetime = new Date(date);
             let hours = datetime.getHours();
@@ -16,9 +29,15 @@ var app = new Vue({
         },
         getLastAccess: function (index) {
             const messages = this.contacts[index].messages;
-            const lastIndex = messages.length - 1;
 
-            return messages[lastIndex].date;
+            if (messages.length) {
+                const lastIndex = messages.length - 1;
+
+                return messages[lastIndex].date;
+            }  else {
+                return '';
+            }
+
         },
         send: function () {
             const nowIndex = this.index
@@ -35,11 +54,14 @@ var app = new Vue({
                 const date = dayjs().format('DD/MM/YYYY HH:mm:ss');
                 const message = {
                     date,
-                    text: 'Ok! Mamy',
+                    text: 'Ok!',
                     status: 'received',
                 }
                 this.contacts[nowIndex].messages.push(message);
             }, 1000);
+        },
+        deleteMsg: function (indexMess) {
+            this.contacts[this.index].messages.splice(indexMess, 1);
         }
     }
 });
